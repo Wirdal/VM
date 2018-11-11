5f6882c977ab8d953dcee6c6d997b0a35bba09a6
Bllarrison <bllarrison@ucdavis.edu>
2018-11-11 08:15:28 -0800

    This should have been a week ago.  Am I weak or something.  Thread.so fully working w/ priorities

--- proj2/src/VirtualMachine.cpp
+++ proj2/src/VirtualMachine.cpp
@@ -126,50 +126,49 @@ void TCBList::ThreadCreate(TVMThreadEntry entry, void *param, TVMMemorySize mems
 }
 
 void TCBList::Sleep(TVMTick tick){
-    VMPrint("Sleep member start\n");
+    //VMPrint("Sleep member start\n");
     if (DCurrentTCB == NULL){
         // Main has been told to go to sleep
         GLOBAL_TICK = tick;
         while (GLOBAL_TICK > 0);
-        VMPrint("Main Sleeping\n");
+        //VMPrint("Main Sleeping\n");
         return;
     }
     else{
-        VMPrint("Thread has been told to go to sleep\n");
+        //VMPrint("Thread has been told to go to sleep\n");
     }
     DCurrentTCB->DTicks=tick;
     while (DCurrentTCB->DTicks > 0);
     DSleepingList.push_back(DCurrentTCB);
     DCurrentTCB = NULL;
-    std::cout<<"ticks: "<< tick << "\n";
-    VMPrint("Sleep member fn end\n");
+    //std::cout<<"ticks: "<< tick << "\n";
+    //VMPrint("Sleep member fn end\n");
 }
 
 void TCBList::ThreadActivate(TVMThreadID thread){
-    VMPrint("/ThreadActivate member fn start\n");
-    std::cout<<"/Activating id: " << thread << " through mFn";
-    TCB* foundtcb = GLOBAL_TCB_LIST.FindTCB(thread);
+
     
-    if (foundtcb == NULL){
-        VMPrint("/Found NULL TCB\n");
-        //return VM_STATUS_ERROR_INVALID_ID;
-    }
-    else{
-        VMPrint("/Found a TCB\n");
-    }
-    if (GLOBAL_TCB_LIST.DCurrentTCB == NULL){
-        VMPrint("DCurrentTCB is NULL");
-    }
-    else{
-        VMPrint("Found a DCurrentTCB");
-    }
-    if (foundtcb ->DState != VM_THREAD_STATE_DEAD) {
-        VMPrint("/Found non dead thread\n");
-        //return VM_STATUS_ERROR_INVALID_STATE;
-    }
-    VMPrint("/Context create\n");
+    //TO OUTPUT INFO
+//    if (foundtcb == NULL){
+//        VMPrint("/Found NULL TCB\n");
+//        //return VM_STATUS_ERROR_INVALID_ID;
+//    }
+//    else{
+//        VMPrint("/Found a TCB\n");
+//    }
+//    if (GLOBAL_TCB_LIST.DCurrentTCB == NULL){
+//        VMPrint("DCurrentTCB is NULL");
+//    }
+//    else{
+//        VMPrint("Found a DCurrentTCB");
+//    }
+//    if (foundtcb ->DState != VM_THREAD_STATE_DEAD) {
+//        VMPrint("/Found non dead thread\n");
+//        //return VM_STATUS_ERROR_INVALID_STATE;
+//    }
+
+    TCB* foundtcb = GLOBAL_TCB_LIST.FindTCB(thread);
     MachineContextCreate(&GLOBAL_TCB_LIST.FindTCB(thread)->DContext, GLOBAL_TCB_LIST.FindTCB(thread)->DEntry, GLOBAL_TCB_LIST.FindTCB(thread)->DParam, GLOBAL_TCB_LIST.FindTCB(thread)->DStack, GLOBAL_TCB_LIST.FindTCB(thread)->DMemsize);
-    VMPrint("/Context created\n");
     
     // Check if the thread needs to yield
     if (foundtcb->DState > GLOBAL_TCB_LIST.DCurrentTCB->DState) {
@@ -179,72 +178,62 @@ void TCBList::ThreadActivate(TVMThreadID thread){
         GLOBAL_TCB_LIST.DCurrentTCB = foundtcb;
     }
     else{
-        VMPrint("/Thread does not need to yield\n");
+        //VMPrint("/Thread does not need to yield\n");
+    }
+    
+    if ((GLOBAL_TCB_LIST.DCurrentTCB->DState == VM_THREAD_STATE_RUNNING) && (GLOBAL_TCB_LIST.DCurrentTCB->DPrio < foundtcb->DPrio)){
+        //VMPrint("/Activate to scheduler, running and current prio < found tcb \n");
+        GLOBAL_TCB_LIST.Schedule();
+    }
+    else if(GLOBAL_TCB_LIST.DCurrentTCB->DState != VM_THREAD_STATE_RUNNING){
+        //VMPrint("/Activate to scheduler, current not running \n");
+        GLOBAL_TCB_LIST.Schedule();
+    }
+    else{
+       // VMPrint("/No schedule necessary \n");
     }
     
     foundtcb->DState = VM_THREAD_STATE_READY;
     GLOBAL_TCB_LIST.AddToReady(foundtcb);
-    VMPrint("/ThreadActivate member fn end\n");
+    //VMPrint("/ThreadActivate member fn end\n");
 }
 
 void TCBList::Threads(){
     //create main thread
-
-    std::cout << "IDLE" << " | TCB ID: " << GLOBAL_TCB_LIST.FindTCB(IDLE_ID)->DTID << "\n"; //should be 1
+    //std::cout << "IDLE" << " | TCB ID: " << GLOBAL_TCB_LIST.FindTCB(IDLE_ID)->DTID << "\n"; //should be 1
     GLOBAL_TCB_LIST.DCurrentTCB = GLOBAL_TCB_LIST.FindTCB(MAIN_ID);
-    std::cout << "Current TCB: "<< GLOBAL_TCB_LIST.FindTCB(MAIN_ID)->DTID << "|" <<GLOBAL_TCB_LIST.DCurrentTCB->DTID << "\n";
+    //std::cout << "Current TCB: "<< GLOBAL_TCB_LIST.FindTCB(MAIN_ID)->DTID << "|" <<GLOBAL_TCB_LIST.DCurrentTCB->DTID << "\n";
 }
 
 void TCBList::Schedule(){
     //Look into the priorities, and switch context based on the results.
     //Also set the current TCB properly
     //Do I need to pop though?
-    VMPrint("Schedule\n");
-    TCB* foundtcb;
-    TCB* tempCurrent; //we need a temporary TCB because we are trying to change current TCB, but still need to store the old one for the context switch.
-    if (GLOBAL_TCB_LIST.DCurrentTCB == NULL){
-        VMPrint("null\n");
-        
 
-    }
-    else{
-        VMPrint("not null\n");
-//        foundtcb = GLOBAL_TCB_LIST.DCurrentTCB; //cannot call current TCB
-//        tempCurrent = GLOBAL_TCB_LIST.DCurrentTCB;  //put the current TCB in a temp TCB
-//        GLOBAL_TCB_LIST.DCurrentTCB = foundtcb; //set the current TCB to the thread we switch to
-        
-        
-        //std::cout << "Switching from " << tempCurrent->DTID< " to " << GLOBAL_TCB_LIST.DCurrentTCB; << "\n";
-        //MachineContextSwitch(&(foundtcb->DContext),&(foundtcb->DContext)); //use both for the context switch
-       
-    }
-    
-    /* - I think these give errors because they check an empty list
-    if (DHighPrio.front() != NULL){
+    // - This chunk gave my machine some pretty serious errors, ended up having to restart my machine to get it to work again, it was checking to see if the queue was empty by comparing it to NULL, fixed by checking if empty instead of comparing with NULL.
+    //VMPrint("The Scheduler Begins \n");
+    TCB* foundtcb;
+    if (!GLOBAL_TCB_LIST.DHighPrio.empty()){    //http://www.cplusplus.com/reference/queue/queue/empty/
         VMPrint("High priority found \n");
         foundtcb = FindTCB(DHighPrio.front()->DTID);
         DHighPrio.pop();
     }
-    else if (DMedPrio.front() != NULL){
+    else if (!GLOBAL_TCB_LIST.DMedPrio.empty()){
         VMPrint("Med priority found \n");
         foundtcb = FindTCB(DMedPrio.front()->DTID);
         DMedPrio.pop();
     }
-    else if (DLowPrio.front() != NULL){
+    else if (!GLOBAL_TCB_LIST.DLowPrio.empty()){
         VMPrint("Low priority found \n");
         foundtcb = FindTCB(DLowPrio.front()->DTID);
         DLowPrio.pop();
     }
     else{
-        VMPrint("Nothing found \n");
+        //VMPrint("Nothing found, push current thread\n");
+        GLOBAL_TCB_LIST.AddToReady(GLOBAL_TCB_LIST.DCurrentTCB);
         //get the idle thread
         // foundtcb = IDLE_TCB;
-        
     }
-     */
-    //VMPrint("The Scheduler Ends \n");
-   
-
 }
 void TCBList::DecrementSleep(){
     int i = 0;
@@ -270,7 +259,6 @@ void Skeleton(void* param){ //The skeleton is just the entry fn you give to the 
     VMThreadTerminate(GLOBAL_TCB_LIST.DCurrentTCB->DTID);
 }
 
-
 /*****************************
  * Our definitions are here  *
  *     Callback Functions    *
@@ -284,7 +272,7 @@ void AlarmCallback(void *calldata){
     //std::cout << "Dticks: "<<GLOBAL_TCB_LIST.DCurrentTCB->DTicks<<"\n";
     //GLOBAL_TCB_LIST.Schedule();
 }
-int mCallbackTick;
+int mCallbackTick; //This is just used to debug so we can see when the Machine Callback gets called without having it flood the screen
 void MachineCallback(void *calldata, int result){
     //MachineEnableSignals();
     mCallbackTick++;
@@ -292,7 +280,6 @@ void MachineCallback(void *calldata, int result){
         //VMPrint(".\n");
         mCallbackTick = mCallbackTick - 5;
     }
-    
     //GLOBAL_TCB_LIST.Schedule();
 }
 void IdleCallback(void* calldata){
@@ -328,21 +315,14 @@ TVMStatus VMStart(int tickms, int argc, char *argv[]){
     MachineRequestAlarm(1000 * tickms, AlarmCallback, NULL);
     MachineEnableSignals();
     
-    VMThreadCreate(NULL, NULL, 0x100000, ((TVMThreadPriority)0x01), &MAIN_ID); //ID should be 2
+    VMThreadCreate(NULL, NULL, 0x100000, VM_THREAD_PRIORITY_HIGH, &MAIN_ID); //ID should be 2
     
     //create idle thread
     VMThreadCreate(IdleCallback, NULL, 0x100000, ((TVMThreadPriority)0x00), &IDLE_ID); //ID should be 1
     
-    //GLOBAL_TCB_LIST.DCurrentTCB = GLOBAL_TCB_LIST.FindTCB(MAIN_ID);
-    
-    //std::cout << "Current TCB: "<< GLOBAL_TCB_LIST.FindTCB(MAIN_ID)->DTID << "|" <<GLOBAL_TCB_LIST.DCurrentTCB->DTID << "\n";
-
+    //GLOBAL_TCB_LIST.DCurrentTCB = GLOBAL_TCB_LIST.FindTCB(MAIN_ID); //This was giving me problems because it seemed to not have access to the data, not sure, reorganized it to be in Thread membmer fn, seen below
     GLOBAL_TCB_LIST.Threads();
-    //set current thread to main
-    //GLOBAL_TCB_LIST.DCurrentTCB = GLOBAL_TCB_LIST.FindTCB(IDLE_ID);
-    //std::cout << "Main" << " | TCB ID: " << GLOBAL_TCB_LIST.FindTCB(MAIN_ID)->DTID << "|" <<GLOBAL_TCB_LIST.DCurrentTCB->DTID << "\n"; //should be 1
-    //std::cout << "IDLE" << " | TCB ID: " << GLOBAL_TCB_LIST.FindTCB(IDLE_ID)->DTID << "\n"; //should be 1
-    
+
     TVMMainEntry entry = VMLoadModule(argv[0]);
     entry(argc, argv);
     //VMUnloadModule();
@@ -380,8 +360,8 @@ TVMStatus VMThreadDelete(TVMThreadID thread){
 }
 
 TVMStatus VMThreadActivate(TVMThreadID thread){
-    std::cout << "std::cout VMThreadActivate ID: "<<thread<<"\n";
-    VMPrint("VMThreadActivate\n");
+    //std::cout << "std::cout VMThreadActivate ID: "<<thread<<"\n";
+    //VMPrint("VMThreadActivate\n");
     TMachineSignalState localsigs;
     MachineSuspendSignals(&localsigs);
     GLOBAL_TCB_LIST.ThreadActivate(thread);
@@ -400,7 +380,7 @@ TVMStatus VMThreadTerminate(TVMThreadID thread){
 
 TVMStatus VMThreadID(TVMThreadIDRef threadref){
     //std::cout << "VMThreadID\n";
-    VMPrint("VMThreadID");
+    //VMPrint("VMThreadID");
     TMachineSignalState localsigs;
     MachineSuspendSignals(&localsigs);
     *threadref = GLOBAL_TCB_LIST.DCurrentTCB->DTID;
@@ -409,7 +389,7 @@ TVMStatus VMThreadID(TVMThreadIDRef threadref){
 
 TVMStatus VMThreadState(TVMThreadID thread, TVMThreadStateRef stateref){
     //std::cout << "VMThreadState\n";
-    VMPrint("VMThreadState\n");
+    //VMPrint("VMThreadState\n");
     //TMachineSignalState localsigs;
     //MachineSuspendSignals(&localsigs);
     TCB* foundtcb = GLOBAL_TCB_LIST.FindTCB(thread);
